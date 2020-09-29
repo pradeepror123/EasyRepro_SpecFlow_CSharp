12903d99d47b9f8f960476a06ec5ec69ada39235
Pradeep Ravella <pravella@ultimatemedical.edu>
2020-09-29 14:37:43 -0500

    Sample Project for ADO

--- ToBeDeleted/UnitTest1.cs
+++ ToBeDeleted/UnitTest1.cs
@@ -2,20 +2,29 @@
 using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium.Chrome;
+using Microsoft.Dynamics365.UIAutomation.Api;
+using Microsoft.Dynamics365.UIAutomation.Sample;
 
 namespace ToBeDeleted
 {
     [TestClass]
     public class UnitTest1
     {
+
+
         [TestMethod]
         public void TestMethod1()
         {
-            var driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("https://docs.microsoft.com/en-us/");
-            driver.Manage().Window.Maximize();
+            var option = TestSettings.Options;
+            var xrmBrowser = new Microsoft.Dynamics365.UIAutomation.Api.Browser(TestSettings.Options);
             Thread.Sleep(5000);
-            driver.Quit();
+            xrmBrowser.Dispose();
+
+            //var driver = new ChromeDriver();
+            //    driver.Navigate().GoToUrl("https://docs.microsoft.com/en-us/");
+            //    driver.Manage().Window.Maximize();
+            //    Thread.Sleep(5000);
+            //    driver.Quit();
             Console.WriteLine("driver opened and closed");
         }
     }
