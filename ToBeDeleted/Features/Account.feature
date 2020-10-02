3e181bbe094e814d1ef77388df0208c2bb3c804d
Pradeep Ravella <pravella@ultimatemedical.edu>
2020-10-02 12:12:40 -0500

    ToBe delted Project added

---
+++ ToBeDeleted/Features/Account.feature
@@ -0,0 +1,9 @@
+﻿Feature: Accounts
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@Tobe
+Scenario: It is a sample Scenario
+	Given user opened browser
+
---
+++ ToBeDeleted/Steps/AccountSteps.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+using TechTalk.SpecFlow;
+
+namespace ToBeDeleted.Steps
+{
+    [Binding]
+    public class AccountsSteps
+    {
+        [Given(@"user opened browser")]
+        public void GivenUserOpenedBrowser()
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://docs.microsoft.com/en-us/");
+            driver.Manage().Window.Maximize();
+            Thread.Sleep(5000);
+            driver.Quit();
+        }
+    }
+}
